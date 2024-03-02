c5b0620db091416b96df5724be86d88d3dd1f732
Josh Joby <jobyjosh21@gmail.com>
2024-03-02 18:45:57 +0530

    updated api endpoint

--- public/client.js
+++ public/client.js
@@ -321,7 +321,7 @@ function sendAudioFiles() {
   });
 
   // Send HTTP POST request
-  fetch('http://127.0.0.1:8080/upload_audio', {
+  fetch('https://synthspeechapi.azurewebsites.net/upload_audio', {
     method: 'POST',
     body: formData
   })
